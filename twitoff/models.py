f516202b9f6077f5453d40e2fbd3cd444cfce920
skhabiri <shahnam.khabiri@gmail.com>
2020-08-18 15:03:09 -0700

    twitter API

--- twitoff/models.py
+++ twitoff/models.py
@@ -8,26 +8,20 @@ class User(DB.Model):
     """Twitter users corresponding to Tweets."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    # Tweet IDs are ordinal ints, so can be used to fetch only more recent
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return '-User {}-'.format(self.name)
 
-
+# Tweet class can have a user argument
 class Tweet(DB.Model):
     """Tweet text and data."""
     id = DB.Column(DB.BigInteger, primary_key=True)
-    text = DB.Column(DB.Unicode(300))  # Allows for text + links, tweet max length=280+ some link length
+    text = DB.Column(DB.Unicode(300))  # Allows for text + links
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
         return '-Tweet {}-'.format(self.text)
-
-
-def insert_example_users():
-    """Example data to play with."""
-    austen = User(id=1, name='austen')
-    elon = User(id=2, name='elonmusk')
-    DB.session.add(austen)
-    DB.session.add(elon)
-    DB.session.commit()
